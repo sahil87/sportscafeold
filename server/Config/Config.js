e1b0f1c23708dda6a58c4c9fd12e6fab6fe14f2b
Mithun Das <mithun.programmer@gmail.com>
2015-07-09 14:54:15 +0530

    Initial setup with socket.io

---
+++ server/Config/Config.js
@@ -0,0 +1,63 @@
+module.exports = {
+    db: {
+        local: {
+            host: 'localhost',
+            port: 27017,
+            dbName: 'fondoo'
+        },
+        remote: {
+            host: 'remote.host',
+            port: 27017,
+            dbName: 'remote-dbname',
+            options: {
+                //TODO change credentials
+                username: 'remote-user',
+                password: 'remote-pass'
+            }
+        }
+    },
+    security: {
+        encryptions: {
+            encrypt_type: 'sha256',
+            encrypt_key: '4b 8?((~FKnpD))>8kb!B |#-uXIO24G3rc:&MG+FR{x;r#Uq4k{Ef@F4E9^-qS!', //change hash key
+        },
+        api: {
+            appId: 'sportscafe', 
+            appSecret: 'sportsTMZaFam59d@F9c#V1G9UEL17)Odzcafe', 
+        },
+        tokenLife: 3600
+    },
+    mailer: {
+        auth: {
+         user: 'test@example.com',
+         pass: 'secret',
+       },
+       defaultFromAddress: 'First Last <test@examle.com>'
+    },
+    cloudinary: {
+        //setttings for the multer options
+        cloud_name: 'cloud-name', 
+        api_key: 'api-key-here', 
+        api_secret: 'api-secret-here' 
+    },
+    gcm: {
+        apiKey: "GCM-Api-key-here"
+    },
+
+    apn: {
+        connection: {
+            gateway: "gateway.push.apple.com",
+            cert: "./cert/pushcert.pem",
+            key: "./cert/pushcert.pem"
+        },
+        feedback: {
+            address: "feedback.push.apple.com",
+            cert: "./cert/pushcert.pem",
+            key: "./cert/pushcert.pem",
+            interval: 43200,
+            batchFeedback: true
+        }
+    },
+    debug         : false,
+    apiVersion    : 'v1.0'
+}
---
+++ server/routers.js
@@ -0,0 +1,70 @@
+/**
+* Initializing the Router essentials
+**/
+var express     = require('express'),
+    Passport    = require('passport'),
+    Auth        = require('./Component/Auth'),
+    User        = require('./Controller/UsersController'),
+    Device      = require('./Controller/DevicesController'),
+    queryParser = require('./Component/QueryParser'),
+    multiparty = require('connect-multiparty'),
+    multipartyMiddleware = multiparty(),
+    server         = express();
+
+/** Created an instance of the express Router **/
+var router = express.Router();
+
+/** Initialize Passport.js **/
+server.use(Passport.initialize());
+
+/** ROUTER DECLARATION STARTS HERE **/
+
+router.get('/unauthorized', function(req, res) {
+  res.json({ message: "The request is Unauthorized" });
+});
+
+router.get('/', function (req, res) {
+    res.json({message: 'API v. 1.0'});
+});
+
+/** USER ROUTES **/
+
+router.route('/users')
+  .post(Auth.isAuthenticated, User.postUsers)
+  .get(Auth.isAuthenticated, User.getUsers);
+
+//router.route('/users/nophotos/find')
+//  .get(Auth.isAuthenticated, User.getUserWithoutPhoto);
+
+router.route('/users/:id')
+  .get(Auth.isAuthenticated, User.getUser)
+  .put(Auth.isAuthenticated, User.updateUser)  // shall be used for all user edit operations, like change password, profile edit etc..
+  .delete(Auth.isAuthenticated, User.deleteUser);
+
+router.route('/users/facebook/:facebookId')
+  .get(Auth.isAuthenticated, User.getUserByFbId);
+
+router.route('/users/:userId/devices')
+  .post(Auth.isAuthenticated, Device.registerDevice);
+
+router.route('/devices')
+  .get(Auth.isAuthenticated, Device.getAllDevices);
+
+router.route('/devices/test') // takes created as query param if needed
+  .get(Auth.isAuthenticated, Device.testDevice);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/** Expose router to other modules **/
+module.exports = router;
